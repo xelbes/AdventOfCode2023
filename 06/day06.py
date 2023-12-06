c9bc074688d3ca2e6b5fcf1d3448569eac07fdc9
slex <slex@incubedit.com>
2023-12-06 10:57:37 +0100

    formated more beautiful

--- 06/day06.py
+++ 06/day06.py
@@ -1,8 +1,6 @@
 input_times = [61, 70, 90, 66]
 input_distance_to_beat = [643, 1184, 1362, 1041]
-
 result = 1
-
 for index, value in enumerate(input_times):
     cnt = 0
     for i in range(1, input_times[index] + 1):
@@ -14,10 +12,6 @@ print(result)
 
 input_times = [61709066]
 input_distance_to_beat = [643118413621041]
-
-input_times_test = [71530]
-input_distance_to_beat_test = [940200]
-
 cnt = 0
 for i in range(1, input_times[0] + 1):
     distance = (input_times[0] - i) * i
