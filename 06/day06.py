a07d8483d4f7434398dde66585b36d2f247d7e5c
slex <slex@incubedit.com>
2023-12-06 15:30:01 +0100

    added time checking for brute force

--- 06/day06.py
+++ 06/day06.py
@@ -1,3 +1,6 @@
+from time import time
+
+time_part_1 = time()
 input_times = [61, 70, 90, 66]
 input_distance_to_beat = [643, 1184, 1362, 1041]
 result = 1
@@ -9,7 +12,9 @@ for index, value in enumerate(input_times):
             cnt += 1
     result *= cnt
 print(result)
+print("Time Part 1:", round(time() - time_part_1, 5))
 
+time_part_2 = time()
 input_times = [61709066]
 input_distance_to_beat = [643118413621041]
 cnt = 0
@@ -18,3 +23,4 @@ for i in range(1, input_times[0] + 1):
     if distance > input_distance_to_beat[0]:
         cnt += 1
 print(cnt)
+print("Time Part 2:", round(time() - time_part_2, 2))
